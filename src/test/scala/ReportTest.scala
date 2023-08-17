916d7372ec5887feff435c539d9b93f5025b520c
Vera Nikalaikina <nikolajkina@gmail.com>
2023-08-17 22:48:11 +0100

    More units

--- src/test/scala/ReportTest.scala
+++ src/test/scala/ReportTest.scala
@@ -1,15 +1,16 @@
 package com.github.nikalaikina
 
-import cats.effect.{Concurrent, IO, Temporal}
-import com.github.nikalaikina.model.*
-import com.github.nikalaikina.repo.*
-import com.github.nikalaikina.service.OrdersReport
-import org.scalacheck.Prop.forAll
-import org.scalacheck.Properties
-import io.github.martinhh.derived.scalacheck.given
+import model.*
+import repo.*
+import service.OrdersReport
+
 import cats.effect.unsafe.implicits.global
-import java.time.Instant
+import cats.effect.{Concurrent, IO, Temporal}
+import io.github.martinhh.derived.scalacheck.given
 import org.scalacheck.Prop.{forAll, propBoolean}
+import org.scalacheck.Properties
+
+import java.time.Instant
 
 object ReportTest extends Properties("OrderReport") {
 
@@ -45,10 +46,78 @@ object ReportTest extends Properties("OrderReport") {
 
         val map = report.unsafeRunSync()
 
-
-        println(map)
         map.size == 1
       }
+  }
+
+  property("everything is counted") = forAll {
+    (
+        orders: List[Order],
+        product: Product,
+        from: Instant,
+        to: Instant
+    ) =>
+      orders.nonEmpty ==> {
+        val ordersFixed = orders.map(absQuantity)
+
+        val ordersRepo: OrderRepo[IO] = new OrderRepo[IO] {
+          override def find(
+              from: Instant,
+              to: Instant
+          ): fs2.Stream[IO, Order] = {
+            fs2.Stream.emits(ordersFixed)
+          }
+        }
+        val productsRepo: ProductRepo[IO] = new ProductRepo[IO] {
+          override def find(ids: Set[ProductId]): fs2.Stream[IO, Product] = {
+            fs2.Stream.emits(
+              ids.toList.map(id => product.copy(id = id))
+            )
+          }
+        }
+
+        val report: IO[Map[Interval, BigInt]] =
+          new OrdersReport[IO](ordersRepo, productsRepo).report(from, to)
+
+        val map = report.unsafeRunSync()
+
+        val sum =
+          ordersFixed.flatMap(_.items).map(_.quantity).map(BigInt.apply).sum
+
+        map.values.sum == sum
+      }
+  }
+
+  property("no orders") = forAll {
+    (
+        product: Product,
+        from: Instant,
+        to: Instant,
+        productCreationDate: Instant
+    ) =>
+      val ordersRepo: OrderRepo[IO] = new OrderRepo[IO] {
+        override def find(
+            from: Instant,
+            to: Instant
+        ): fs2.Stream[IO, Order] = {
+          fs2.Stream.empty
+        }
+      }
+      val productsRepo: ProductRepo[IO] = new ProductRepo[IO] {
+        override def find(ids: Set[ProductId]): fs2.Stream[IO, Product] = {
+          fs2.Stream.emits(
+            ids.toList.map(id =>
+              product.copy(id = id, creationDate = productCreationDate)
+            )
+          )
+        }
+      }
+
+      val report: IO[Map[Interval, BigInt]] =
+        new OrdersReport[IO](ordersRepo, productsRepo).report(from, to)
+
+      val map = report.unsafeRunSync()
+      map.isEmpty
   }
 
   private def absQuantity(o: Order): Order =
